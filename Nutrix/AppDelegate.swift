96b33d562ce8aa676dbbe80a3c6ba4c4b0c3131f
Charles Xu <xuzheyuan961124@gmail.com>
2021-01-11 19:19:36 -0800

    finished register part

--- Nutrix/AppDelegate.swift
+++ Nutrix/AppDelegate.swift
@@ -6,11 +6,16 @@
 //
 
 import UIKit
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    override init() {
+        FirebaseApp.configure()
+    }
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
--- Nutrix/Controllers/registerViewController.swift
+++ Nutrix/Controllers/registerViewController.swift
@@ -6,16 +6,60 @@
 //
 
 import UIKit
+import GoogleSignIn
+import Firebase
+import AuthenticationServices
 
 class registerViewController: UIViewController {
+    
+    //Declare database instance
+    let db = Firestore.firestore()
+    
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var registeredField: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+        registeredField.isHidden = true
+    }
+    
+    
+    func addUsername(withName username: String, withEmail email: String) {
+        var ref: DocumentReference? = nil
+        ref = db.collection("usersInfo").addDocument(data: [
+            "username": username,
+            "email": email
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
     }
     
     @objc func dismissKeyboard() {
         view.endEditing(true)
+    }
+    
+    @IBAction func registerAction(_ sender: UIButton) {
+        let email = emailField.text!
+        let password = passwordField.text!
+        let username = usernameField.text!
+        
+        addUsername(withName: username, withEmail: email)
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            // [START_EXCLUDE]
+          guard let user = authResult?.user, error == nil else {
+            return
+          }
+          print("\(user.email!) created")
+        }
+        registeredField.isHidden = false
     }
 }
